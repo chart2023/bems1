33446451b5026b99dd019e1c6bf3484013ddb9d9
chart2023 <chart2023@gmail.com>
2016-02-02 14:50:18 +0700

    Update manage1.sh

--- manage1.sh
+++ manage1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo mkdir /var/www/html/bem1/
 sudo cp -r /opt/openbaton/scripts/assets/ /opt/openbaton/scripts/index.php /var/www/html/bem1
+sudo touch /opt/openbaton/test.txt
+sudo echo $server_private > /opt/openbaton/test.txt
